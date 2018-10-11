e987faf235e7619fcf3db5a6793be6541214d199
kaiserb <mbarrie07@gmail.com>
2018-10-10 20:55:33 -0400

    Initial commit

---
+++ build/default/debug/SGprojSerialChoose_transmit.p1.d
@@ -0,0 +1,3 @@
+ build/default/debug/SGprojSerialChoose_transmit.d  \
+ build/default/debug/SGprojSerialChoose_transmit.p1:  \
+ SGprojSerialChoose_transmit.c 
---
+++ build/default/production/SGprojSerialChoose_transmit.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/SGprojSerialChoose_transmit.d  \
+ build/default/production/SGprojSerialChoose_transmit.p1:  \
+ SGprojSerialChoose_transmit.c 
